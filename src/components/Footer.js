30bc884d1726952b9425d2d10125c2b837005764
OlgaGabbert <olga.gabbert@gmail.com>
2023-11-22 12:42:32 +0100

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p>&copy; 2023 Olga Gabbert</p>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
